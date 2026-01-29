9cf910a552d4074fecf260ec00fe1b757f3c00f5
VAxxel <VinceAxelGD@gmail.com>
2026-01-29 13:23:41 -0600

    Primer ejercicio de nivel 2

---
+++ src/main/java/com/pruebasspark/level2/ExampleNivel2.java
@@ -0,0 +1,56 @@
+package com.pruebasspark.level2;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.col;
+
+
+public class ExampleNivel2 {
+    public static void main(String[] args) {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "error");
+
+        SparkSession spark = SparkSession.builder()
+                .appName("EjemploNivel2")
+                .master("local[*]")
+                .getOrCreate();
+
+        Dataset<Row> dfOriginal = spark.read()
+                .option("header", true)//Le decimos que use la primera fila como nombres de columnas
+                .option("inferSchema", true)//Infiere si es double, String, int, etc.
+                .csv("insumos/ventas.csv");//La ruta donde se encuentra el archivo
+
+        dfOriginal.show();
+
+        //Eliminar columnas donde una columna especifica sea nula
+        Dataset<Row> dfLimpio = dfOriginal.na().drop(new String[]{"fecha"});
+
+        //Rellenar nulos con un valor(ej. 0) en columnas especificas
+        dfLimpio = dfLimpio.na().fill(0,new String[]{"cantidad"});
+        dfLimpio.show();
+
+        //Creamos la columna "Total" multiplicando cantidad por precio
+        dfLimpio = dfLimpio.withColumn("Total",col("cantidad").multiply(col("precio_unitario")));
+
+        System.out.println("--- Tabla con fechas sin null y cantidad 0 ---");
+        dfLimpio.show();
+
+        //Salida de CSV com caracteristicas pedidas
+        System.out.println("--- Salida del CSV ---");
+        dfLimpio.write()
+                .mode("overwrite")//El mode Overwrite sobrescribe la carpeta si ya existe, es util para las pruebas
+                .option("header",true)
+                .csv("salidas/SalidaVentasCsv");
+
+        System.out.println("--- Salida de Json ---");
+        dfLimpio.write()
+                .mode("overwrite")//El mode Overwrite sobrescribe la carpeta si ya existe, es util para las pruebas
+                .json("salidas/SalidaVentasJson");
+
+        System.out.println("--- Salida de Parquet ---");
+        dfLimpio.write()
+                .mode("overwrite")//El mode Overwrite sobrescribe la carpeta si ya existe, es util para las pruebas
+                .parquet("salidas/SalidaVentasParquet");
+    }
+}
